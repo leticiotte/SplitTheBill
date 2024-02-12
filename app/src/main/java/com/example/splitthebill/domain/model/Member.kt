f470458756c88bdd527b2a3eb0bebba52b331e61
Letícia Gonçalves <leticiaespregagon@gmail.com>
2024-02-12 20:17:11 -0300

    feat: adds logical to add new member

--- app/src/main/java/com/example/splitthebill/domain/model/Member.kt
+++ app/src/main/java/com/example/splitthebill/domain/model/Member.kt
@@ -2,13 +2,14 @@ package com.example.splitthebill.domain.model
 
 import java.io.Serializable
 
-class Member(index: Int?, name: String, value: Double, list: MutableList<Item>) : Serializable {
-    var index: Int?
-    var name: String
+class Member : Serializable {
+    var index: Int? = null
+    var name: String = ""
     var amountPaid: Double = 0.0
-    var items: MutableList<Item>
+    var items: MutableList<Item> = mutableListOf()
+    constructor()
 
-    init {
+    constructor(index: Int?, name: String, value: Double, list: MutableList<Item>) {
         this.index = index
         this.name = name
         this.amountPaid = value
--- app/src/main/java/com/example/splitthebill/ui/fragment/MemberFormFragment.kt
+++ app/src/main/java/com/example/splitthebill/ui/fragment/MemberFormFragment.kt
@@ -4,20 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.splitthebill.R
 import com.example.splitthebill.databinding.FragmentMemberFormBinding
+import com.example.splitthebill.domain.model.Item
+import com.example.splitthebill.domain.model.Member
+import com.example.splitthebill.domain.repository.MemberRepository
+import com.google.android.material.snackbar.Snackbar
+import org.koin.android.ext.android.get
 
 class MemberFormFragment : Fragment() {
     private var _binding: FragmentMemberFormBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var addMemberNameEt: EditText
     private lateinit var addMemberHasItemCb: CheckBox
     private lateinit var addMemberItemDescriptionEt: EditText
     private lateinit var addMemberItemValueEt: EditText
+    private lateinit var saveBtn: Button
+
+    private val memberRepository: MemberRepository = get()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,7 +36,9 @@ class MemberFormFragment : Fragment() {
     ): View? {
         _binding = FragmentMemberFormBinding.inflate(inflater, container, false)
 
+        setupInputs()
         setupHasItemCb()
+        setupSaveBtnOnClickListener()
         return binding.root
     }
 
@@ -34,11 +47,15 @@ class MemberFormFragment : Fragment() {
         _binding = null
     }
 
-    private fun setupHasItemCb(){
+    private fun setupInputs() {
+        addMemberNameEt = binding.addMemberNameEt
         addMemberHasItemCb = binding.addMemberHasItemCb
         addMemberItemDescriptionEt = binding.addMemberItemDescriptionEt
         addMemberItemValueEt = binding.addMemberItemValueEt
+        saveBtn = binding.saveBtn
+    }
 
+    private fun setupHasItemCb() {
         addMemberHasItemCb.setOnCheckedChangeListener { buttonView, isChecked ->
             if (isChecked) {
                 addMemberItemDescriptionEt.isEnabled = true
@@ -47,11 +64,56 @@ class MemberFormFragment : Fragment() {
                 addMemberItemValueEt.background = null
             } else {
                 addMemberItemDescriptionEt.isEnabled = false
-                addMemberItemDescriptionEt.background = ContextCompat.getDrawable(binding.root.context, R.drawable.edittext_background_disabled)!!
+                addMemberItemDescriptionEt.background = ContextCompat.getDrawable(
+                    binding.root.context,
+                    R.drawable.edittext_background_disabled
+                )!!
                 addMemberItemValueEt.isEnabled = false
-                addMemberItemValueEt.background = ContextCompat.getDrawable(binding.root.context, R.drawable.edittext_background_disabled)!!
+                addMemberItemValueEt.background = ContextCompat.getDrawable(
+                    binding.root.context,
+                    R.drawable.edittext_background_disabled
+                )!!
             }
         }
     }
 
+    private fun setupSaveBtnOnClickListener() {
+        saveBtn.setOnClickListener {
+            if (!isValidInputs()) {
+                Snackbar.make(
+                    binding.root,
+                    "Preencha o formulário corretamente",
+                    Snackbar.LENGTH_LONG
+                )
+                    .setAction("Action", null).show()
+                return@setOnClickListener
+            }
+            createMemberWithFormValues()
+        }
+    }
+
+    private fun isValidInputs(): Boolean {
+        if (addMemberNameEt.text.toString().isNullOrEmpty()) return false
+        if (addMemberHasItemCb.isChecked) {
+            if (addMemberItemDescriptionEt.text.toString().isNullOrEmpty()) return false
+            if (addMemberItemValueEt.text.toString().isNullOrEmpty()) return false
+        }
+        return true
+    }
+
+    private fun createMemberWithFormValues() {
+        var member = Member()
+        member.name = addMemberNameEt.text.toString()
+        if (addMemberHasItemCb.isChecked) {
+            var item = Item(
+                0,
+                addMemberItemDescriptionEt.text.toString(),
+                addMemberItemValueEt.text.toString().toDouble()
+            )
+            member.items.add(item)
+            member.amountPaid = item.value
+        }
+        memberRepository.addMember(member)
+        findNavController().navigate(R.id.action_AddMemberFragment_to_MembersFragment)
+    }
 }
